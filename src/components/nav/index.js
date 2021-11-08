c41a0434c9cd7667534b765f5c2b61d3ab9f1439
adrenalize-brad <brad@adrenalizedigital.ca>
2021-11-08 16:59:51 -0700

    reduce transition speeds

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -142,9 +142,9 @@ const Nav = ({ context, open, navLogo }) => {
                     onClick={() => setMenuLinksOpen(menuLinksOpen === true ? false : true)}
                     className="relative flex flex-col h-12 w-12 ml-auto mr-4 cursor-pointer"
                 >
-                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 h-1 rounded-lg shadow-md transform transition duration-300 ease-in-out ${menuLinksOpen === true ? '-translate-y-1/2 rotate-45 bg-blue-600 w-8' : '-translate-y-3 bg-gray-100 bg-opacity-75 w-12' }` } />
-                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1 rounded-lg shadow-md transform transition duration-200 ease-in-out ${menuLinksOpen === true ? 'scale-0' : 'bg-gray-100 bg-opacity-75 w-12' }` } />
-                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 h-1 rounded-lg shadow-md transform transition duration-300 ease-in-out ${menuLinksOpen === true ? '-translate-y-1/2 -rotate-45 bg-blue-600 w-8' : 'translate-y-2 bg-gray-100 bg-opacity-75 w-12' }` } />
+                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 h-1 rounded-lg shadow-md transform transition duration-150 ease-in-out ${menuLinksOpen === true ? '-translate-y-1/2 rotate-45 bg-blue-600 w-8' : '-translate-y-3 bg-gray-100 bg-opacity-75 w-12' }` } />
+                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 h-1 rounded-lg shadow-md transform transition duration-75 ease-in-out ${menuLinksOpen === true ? 'scale-0' : 'bg-gray-100 bg-opacity-75 w-12' }` } />
+                    <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 h-1 rounded-lg shadow-md transform transition duration-150 ease-in-out ${menuLinksOpen === true ? '-translate-y-1/2 -rotate-45 bg-blue-600 w-8' : 'translate-y-2 bg-gray-100 bg-opacity-75 w-12' }` } />
                 </div>
             : 
                 null 
